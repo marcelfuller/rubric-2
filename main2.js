3e671dfc2aded01cc21f4ac2bbea48cd4988e52e
marcel fuller <Marcel.fuller@outlook.com>
2021-12-01 13:58:45 -0600

    edited
    edited

---
+++ main2.js
@@ -0,0 +1,66 @@
+// JavaScript Document
+var firstName = "marcel fuller"; 
+console.log(this.firstName);
+function myName(){
+	var firstName = "Rudy M";
+	console.log (this.firstName);
+}
+myName();
+
+const Rate= 0.1;
+setTimeout(message,2000)
+
+public class lesson7 {
+	public static void main(string[] args){
+    char grade = 'A';
+	
+	switch(grade){
+	case'A':
+			System.out.println("Excellent job!");
+			break;
+	case'B':
+			System.out.println("good job!");
+			break;
+	case'C':
+			System.out.println("keep trying!");
+			break;
+	case'D':
+			System.out.println("keep studying!");
+			break;
+	case'F':
+			System.out.println("Do better!");
+			break;
+		default:
+			System.out.println("you made a invalid entery")
+			
+	}
+}
+	function variable (){
+	var greeting="hi how are you?"};
+consol.log(greeting); }
+	
+  // code to be executed
+
+	 const myName = {
+  firstName:"Marcel",
+  lastName: "fuller",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+
+	Return data from function 
+	let x= myFunc(5, 3); }  // Function is called, return value will end up in x
+
+function myFunc(a, b) {
+  return a * b;            
+// Function returns the product of a and b
+}
+function testfun(param1, param2); {
+	console.log(param1, param2);
+}
+testfun("Northwest", "Vista");
+
+
+// This will return "Marcel Fuller":
+myObject.fullName();
+*/
